966d8f33f9b3313b4814f883f729ddd36b505483
Rohit Parab <rohit@Rohits-MacBook-Air.local>
2018-04-01 01:50:16 +0530

    Queries

--- database/models/album.js
+++ database/models/album.js
@@ -1,1 +1,15 @@
 // Todo: create Album Schema
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const albumSchema = new Schema({
+  title : String,
+  date  : Date,
+  copiesSold : Number,
+  numberTracks: Number,
+  image : String, //reference to the image
+  revenue : Number
+});
+
+module.exports = albumSchema;
